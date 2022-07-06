c360ad04fb0ed10196c8ec1503cec1b0ef9312ff
Alexandre Santana Dos Santos <89360079+Tr3mbolon4@users.noreply.github.com>
2022-07-06 00:30:34 +0000

    mudar formato

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 print('Codigo de: [Alexandre Santos]')
 print('='*35)
 
-valor = int(input('Digite um valor:'))
+#Ola Agora vamos aprender usar o (for in) com string modificando palavras e colocando espaço entre elas, para da um visual diferente.
+#Nesse código para um visual mais dinâmico vamos usar letras maiúsculas para a palavra, observando essa logica eu poderia fazer o código assim como o professor fez na aula como no exemplo a baixo.
 
-if valor >= 20 and valor < 40:
-  print('Valor aceito')
+#palavra = FANTASTICO
+#for spaco in palavra:
+    #Print(f’ {spaco}’  , end=’’) 
+
+#Dessa forma e o código iria se executado conforme vc mudasse o objeto palavra, porem quem não pratica não aprende, vamos melhorar isso, colocando input para o usuário colocar a palavra que quiser e vamos colocar o comando obrigando transforma toda a palavra digitada em letras maiúsculas 
+
+#dessa forma em baixo.
+
+palavra = input('Digite uma Palavra: ')
+
+for spaco in (palavra.upper()):
+  print(f' {spaco}' , end='')
+
+
+#aqui está o código praticar e tudo sei isso hoje rsrrsrsr  
+        
   
-else:
-  print('valor nao compartivel')
+        
+          
